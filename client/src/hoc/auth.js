185437799d13e1b51982908683b912ad796cbfc7
Maciek <maciej.bolsega@gmail.com>
2022-09-06 17:19:53 +0200

    cleanup

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -10,24 +10,15 @@ export default function (SpecificComponent, option, adminRoute = null) {
         const dispatch = useDispatch();
 
         useEffect(() => {
-            //To know my current status, send Auth request 
             dispatch(auth()).then(response => {
-                //Not Loggined in Status 
-                if (!response.payload.isAuth) {
-                    if (option) {
-                        props.history.push('/login')
-                    }
-                    //Loggined in Status 
+                if (!response.payload.isAuth && option) {
+                    props.history.push('/login')
                 } else {
-                    //supposed to be Admin page, but not admin person wants to go inside
                     if (adminRoute && !response.payload.isAdmin) {
                         props.history.push('/')
                     }
-                    //Logged in Status, but Try to go into log in page 
-                    else {
-                        if (option === false) {
-                            props.history.push('/')
-                        }
+                    else if(option === false){
+                        props.history.push('/')
                     }
                 }
             })
